fc31d65ca79fdbccde33624149392fbf483f7c02
Murvyn <99991623+murvyn@users.noreply.github.com>
2024-07-16 05:30:02 +0000

    refactor client code

--- server/src/routes/message.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { addMessage } from "../controllers/chatController";
-import { auth } from "../middleware/auth";
-
-const router = Router()
-
-router.post('/send-message', auth(["student", "lecturer", "HOD"]), addMessage)
-
-export default router
