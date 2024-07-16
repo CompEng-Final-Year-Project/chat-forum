138fb84d6c29904d1f5aaf963d314d7d4e607137
Murvyn <99991623+murvyn@users.noreply.github.com>
2024-07-16 03:40:13 +0000

    testing

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,7 +16,7 @@ const app = express();
 
 logger;
 routes(app);
-connectDB();
+// connectDB();
 prod(app);
 
 app.get('/api/', (req, res) => {
@@ -24,60 +24,60 @@ app.get('/api/', (req, res) => {
 })
 
 const port = process.env.PORT || 5000;
-const httpServer = createServer(app);
-const io = new Server(httpServer, {
-  cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
-});
+// const httpServer = createServer(app);
+// const io = new Server(httpServer, {
+//   cors: {
+//     origin: "http://localhost:5173",
+//     methods: ["GET", "POST"],
+//     credentials: true,
+//   },
+// });
 
-let onlineUsers: { userId: string; socketId: string }[] = [];
+// let onlineUsers: { userId: string; socketId: string }[] = [];
 
 
-io.on("connection", (socket) => {
-  console.log("A user connected", socket.id);
+// io.on("connection", (socket) => {
+//   console.log("A user connected", socket.id);
 
-  const userId = socket.handshake.query.userId as string | undefined;
+//   const userId = socket.handshake.query.userId as string | undefined;
 
-  if (userId) {
-    if (!onlineUsers.some((user) => user.userId === userId)) {
-      onlineUsers.push({ userId, socketId: socket.id });
-    }
-    console.log("online users", onlineUsers);
+//   if (userId) {
+//     if (!onlineUsers.some((user) => user.userId === userId)) {
+//       onlineUsers.push({ userId, socketId: socket.id });
+//     }
+//     console.log("online users", onlineUsers);
 
-    io.emit("getOnlineUsers", onlineUsers);
+//     io.emit("getOnlineUsers", onlineUsers);
 
-    socket.on("sendMessage", (message) => {
-      const recipientSocket = onlineUsers.find(
-        (user) => user.userId === message.recipientId
-      );
-      if (recipientSocket) {
-        io.to(recipientSocket.socketId).emit("getMessage", message);
-      } else {
-        console.warn("Recipient not found:", message.recipientId);
-        // You can emit an error message to the sender here
-      }
+//     socket.on("sendMessage", (message) => {
+//       const recipientSocket = onlineUsers.find(
+//         (user) => user.userId === message.recipientId
+//       );
+//       if (recipientSocket) {
+//         io.to(recipientSocket.socketId).emit("getMessage", message);
+//       } else {
+//         console.warn("Recipient not found:", message.recipientId);
+//         // You can emit an error message to the sender here
+//       }
 
-    });
-  }
+//     });
+//   }
 
-  socket.on("disconnect", (reason) => {
-    console.log("User disconnected:", reason, socket.id);
-    if (userId) {
-      onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
-      console.log("User disconnected", socket.id);
-    }
-  });
+//   socket.on("disconnect", (reason) => {
+//     console.log("User disconnected:", reason, socket.id);
+//     if (userId) {
+//       onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+//       console.log("User disconnected", socket.id);
+//     }
+//   });
 
-  socket.on("error", (error) => {
-    console.error("Socket error:", error);
-  });
-});
+//   socket.on("error", (error) => {
+//     console.error("Socket error:", error);
+//   });
+// });
   
 
-const server = httpServer.listen(port, () => {
+const server = app.listen(port, () => {
   logger.info(`Listening on port ${port}...`);
 });
 
